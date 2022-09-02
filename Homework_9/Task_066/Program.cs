635ccc7e65f88b5aeea54695bc6cbd558ceab979
iam23db <iam23db@yandex.ru>
2022-09-02 08:47:47 +0300

    homework 9 added

---
+++ Homework_9/Task_066/Program.cs
@@ -0,0 +1,29 @@
+﻿// // Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// четных чисел натуральных элементов в 
+// промежутке от M до N.
+// M = 1; N = 15 -> 56
+// M = 4; N = 8. -> 18
+
+
+
+
+int SummOfNumbers(int m, int n)
+{
+    if (n < m) return 0;
+    else if (m % 2 != 0)
+    {
+        m = m + 1;
+        return m + SummOfNumbers(m + 2, n);
+    }
+    else
+    {
+        return m + SummOfNumbers(m + 2, n);
+    }
+}
+
+
+
+int m = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine(SummOfNumbers(m, n));
---
+++ Homework_9/task_064/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+// M = 1; N = 5. -> ""5, 4, 3, 2, 1""
+// M = 4; N = 8. -> ""8, 7, 6, 5, 4""
+
+
+string PrintNumbers (int m, int n)
+{
+    if (n==m) return $"{m}";
+    else return $"{n}" + PrintNumbers(m, n-1);
+}
+
+
+
+
+int m = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.Write(PrintNumbers(m,n));
